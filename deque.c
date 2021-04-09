027e56b6af577a4596bd3d980d609954db9e33d0
HankyeolKim-SMU <201810927@sangmyung.kr>
2021-04-09 17:26:19 +0900

    deque

---
+++ deque.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_QUEUE_SIZE 5
+
+typedef int element;
+typedef struct{
+    element data[MAX_QUEUE_SIZE];
+    int front;
+    int rear;
+} DequeType;
+
+void error(char *message)
+{
+    fprintf(stderr, "%s\n", message);
+    exit(1);
+}
+
+void init_deque(DequeType *q)
+{
+    q->front = 0;
+    q->rear = 0;
+}
+
+int is_full(DequeType *q)
+{
+    return ((q -> rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+int is_empty(DequeType *q)
+{
+    return (q->front == q->rear);
+}
+
+void deque_print(DequeType *q)
+{
+    printf("deque(front = %d rear = %d) = ", q->front, q->rear);
+    if (!is_empty(q)){
+        int i = q->front;
+        do{
+            i = (i + 1) % (MAX_QUEUE_SIZE);
+            printf("%d | ", q->data[i]);
+            if (i == q->rear)
+                break;
+        }while(i != q->front);
+    }
+    printf("\n");
+}
+
+void add_rear(DequeType *q, element item)
+{
+    if (is_full(q))
+        error("덱이 포화상태입니다.");
+    q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+    q->data[q->rear] = item;
+}
+
+element delete_front(DequeType *q)
+{
+    if (is_empty(q))
+        error("덱이 공백상태입니다.");
+    q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+    return q->data[q->front];
+}
+
+void add_front(DequeType *q, element value)
+{
+    if (is_full(q))
+        error("덱이 포화상태입니다");
+    q->data[q->front] = value;
+    q->front = (q->front - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
+}
+
+element get_front(DequeType *q)
+{
+    if (is_empty(q))
+        error("덱이 공백상태입니다.");
+    return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
+}
+
+element delete_rear(DequeType *q)
+{
+    int prev = q->rear;
+    if (is_empty(q))
+        error("덱이 공백상태입니다.");
+    q->rear = (q->rear - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
+    return q->data[prev];
+}
+
+element get_rear(DequeType *q)
+{
+    if (is_empty(q))
+        error("덱이 공백상태입니다.");
+    return q->data[q->rear];
+}
+int main(void)
+{
+    DequeType deque;
+
+    init_deque(&deque);
+    for(int i = 0; i < 3; i++){
+        add_front(&deque, i);
+        deque_print(&deque);
+    }
+    
+    for(int i = 0; i < 3; i++){
+        delete_rear(&deque);
+        deque_print(&deque);
+    }
+    return 0;
+}
