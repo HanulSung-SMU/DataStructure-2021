25a89a0eb0f8fca8cfccecd79b55d95186771574
HankyeolKim-SMU <201810927@sangmyung.kr>
2021-04-09 17:53:53 +0900

    queue

---
+++ queue_2.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_QUEUE_SIZE 5
+
+typedef int element;
+typedef struct{
+    int front;
+    int rear;
+    element data[MAX_QUEUE_SIZE];
+} QueueType;
+
+void error(char *message)
+{
+    fprintf(stderr, "%s\n", message);
+    exit(1);
+}
+
+void init_queue(QueueType *q)
+{
+    q->rear = 0;
+    q->front = 0;
+}
+
+int is_full(QueueType *q)
+{
+    return ((q -> rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+int is_empty(QueueType *q)
+{
+    return (q->front == q->rear);
+}
+void queue_print(QueueType *q)
+{
+    printf("QUEUE(front = %d rear = %d) = ", q->front, q->rear);
+    if (!is_empty(q)){
+        int i = q->front;
+        do{
+            i = (i + 1) % (MAX_QUEUE_SIZE);
+            printf("%d | ", q->data[i]);
+            if (i == q->rear)
+                break;
+        }while(i != q->front);
+    }
+    printf("\n");
+}
+
+void enqueue(QueueType *q, int item)
+{
+    if (is_full(q))
+        error("큐가 포화상태입니다.");
+    q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+    q->data[q->rear] = item;
+}
+
+int dequeue(QueueType *q)
+{
+    if (is_empty(q))
+        error("큐가 공백상태입니다.");
+    q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+    return q->data[q->front];
+}
+
+int peek(QueueType *q)
+{
+    if(is_empty(q))
+        error("큐가 공백상태입니다.");
+    return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
+}
+
+int main(void)
+{
+    QueueType queue;
+    int number;
+    init_queue(&queue);
+    printf("--데이터 추가 단계--\n");
+    while (!is_full(&queue))
+    {
+        printf("정수를 입력하시오 : ");
+        scanf("%d", &number);
+        enqueue(&queue, number);
+        queue_print(&queue);
+    }
+    printf("큐는 포화상태입니다.\n\n");
+    
+    printf("--데이터 삭제 단계 \n");
+    while(!is_empty(&queue))
+    {
+        number = dequeue(&queue);
+        printf("꺼내진 정수 : %d \n", number);
+        queue_print(&queue);
+    }
+    printf("큐는 공백상태입니다.\n");
+    return 0;
+}
