bfae22cc1d1c794beaac81d1272e6f4f4780506f
SooGiKim-SMU <sg990926@gmail.com>
2021-04-22 19:09:40 +0900

    queue

---
+++ Linear_q.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_QUEUE_SIZE 5
+
+typedef int element;
+typedef struct {
+    int front;
+    int rear;
+    element data [MAX_QUEUE_SIZE];
+} QueueType;
+
+//오류 함수
+void error (char *message) {
+    fprintf(stderr, "%s\n", message);
+    exit(1);
+}
+//큐 초기화 함수
+void init_queue (QueueType *q) {
+    q->rear = -1;
+    q->front = -1;
+}
+//큐 출력 함수
+void queue_print (QueueType *q) {
+    for (int i = 0; i < MAX_QUEUE_SIZE; i++) {
+        if (i <= q->front || i > q->rear)
+            printf("   | ");
+        else
+            printf("%d | ", q->data[i]);
+    }
+    printf("\n");
+}
+//포화 상태 검출
+element is_full (QueueType *q) {
+    if (q->rear == MAX_QUEUE_SIZE - 1)
+        return 1;
+    else
+        return 0;
+}
+//공백 상태 검출
+element is_empty (QueueType *q) {
+    if(q->front == q->rear)
+        return 1;
+    else
+        return 0;
+}
+//큐에 삽입
+void enqueue (QueueType *q, int item) {
+    if (is_full(q)) {
+        error("큐가 포화상태입니다.");
+        return;
+    }
+    q->data[++(q->rear)] = item;
+}
+//큐에서 삭제
+element dequeue (QueueType *q) {
+    if (is_empty(q)) {
+        error("큐가 공백상태입니다.");
+        return -1;
+    }
+    int item = q->data[++(q->front)];
+    return item;
+}
+//메인
+int main (void) {
+    int item = 0;
+    QueueType q;
+    
+    init_queue(&q);
+    
+    enqueue(&q, 10); queue_print(&q);
+    enqueue(&q, 20); queue_print(&q);
+    enqueue(&q, 30); queue_print(&q);
+    enqueue(&q, 40); queue_print(&q);
+    enqueue(&q, 50); queue_print(&q);
+
+    item = dequeue(&q); queue_print(&q);
+    item = dequeue(&q); queue_print(&q);
+    item = dequeue(&q); queue_print(&q);
+    item = dequeue(&q); queue_print(&q);
+    item = dequeue(&q); queue_print(&q);
+
+    enqueue(&q, 10); queue_print(&q);
+
+    return 0;
+}
+
