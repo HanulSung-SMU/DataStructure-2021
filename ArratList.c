94c7384df074a890a61b52db1a7eb9c5484bce79
201311245_정영훈 <tkfkdof12@gmail.com>
2021-04-30 01:26:30 +0900

    remote repository test

--- ArratList.c
+++ ArratList.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_LIST_SIZE 10
+
+#define MAX_LIST_SIZE 100
 typedef int element;
 
-typedef struct{
+typedef struct {
 	element array[MAX_LIST_SIZE];
-	int size;	
+	int size;
 }ArrayListType;
 
 
-void error(char *message){
+void error(char* message) {
 	fprintf(stderr, "%s\n", message);
 	exit(1);
 }
 
 
-void init(ArrayListType *L){
+void init(ArrayListType* L) {
 	L->size = 0;
 }
 
 
-int is_empty(ArrayListType *L){
+int is_empty(ArrayListType* L) {
 	return L->size == 0;
 }
 
 
-int is_full(ArrayListType *L){
+int is_full(ArrayListType* L) {
 	return L->size == MAX_LIST_SIZE;
 }
 
-element get_entry(ArrayListType *L int pos){
+element get_entry(ArrayListType* L, int pos) {
 	if (pos < 0 || pos >= L->size)
 	{
 		error("located error");
@@ -38,36 +39,36 @@ element get_entry(ArrayListType *L int pos){
 	return L->array[pos];
 }
 
-void print_list(ArrayListType *L){
+void print_list(ArrayListType* L) {
 	int i;
 	for (i = 0; i < L->size; i++)
 	{
-		printf("%d->", L->array[i]);
+		printf("%d ->", L->array[i]);
 	}
 	printf("\n");
 }
 
-void insert_last(ArrayListType *L, element item){
+void insert_last(ArrayListType* L, element item) {
 	if (L->size >= MAX_LIST_SIZE)
 	{
 		error("List overflow");
 	}
-	L->array[L->size++]=item;
+	L->array[L->size++] = item;
 }
 
-void insert(ArrayListType *L, int pos, element item){
+void insert(ArrayListType* L, int pos, element item) {
 	if (!is_full(L) && (pos >= 0) && (pos <= L->size))
 	{
 		for (int i = (L->size - 1); i >= pos; i--)
 		{
-			L->array[i+1] = L->array[i];
+			L->array[i + 1] = L->array[i];
 		}
 		L->array[pos] = item;
 		L->size++;
 	}
 }
 
-element delete(ArrayListType *L, int pos){
+element delete(ArrayListType* L, int pos) {
 	element item;
 	if (pos < 0 || pos >= L->size)
 	{
@@ -76,13 +77,13 @@ element delete(ArrayListType *L, int pos){
 	item = L->array[pos];
 	for (int i = pos; i < (L->size - 1); i++)
 	{
-		L->array[i] = L->array[i+1];
+		L->array[i] = L->array[i + 1];
 	}
 	L->size--;
 	return item;
 }
 
-int main(void){
+int main(void) {
 	ArrayListType list;
 
 	init(&list);
