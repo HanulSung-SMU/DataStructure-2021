1c6dfaedf488a28e71846160072a457addd86487
HYERINI <rinlee2001@naver.com>
2021-05-07 14:04:42 +0900

    linked list

---
+++ 이중연결리스트.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct DListNode {
+    element data;
+    struct DListNode* llink;
+    struct DListNode* rlink;
+} DListNode;
+
+void init(DListNode* phead)
+{
+    phead->llink = phead;
+    phead->rlink = phead;
+}
+
+void insert(DListNode* before, element data)
+{
+    DListNode* newnode = (DListNode*)malloc(sizeof(DListNode));
+    newnode->data = data;
+    newnode->llink = before;
+    newnode->rlink = before->rlink;
+    before->rlink->llink = newnode;
+    before->rlink = newnode;
+}
+
+void delete(DListNode* head, DListNode* removed)
+{
+    if (removed == head) return;
+    removed->llink->rlink = removed->rlink;
+    removed->rlink->llink = removed->llink;
+    free(removed);
+}
+
+void print_list(DListNode* phead)
+{
+    DListNode* p;
+    for (p = phead->rlink; p != phead; p = p->rlink) {
+        printf("<-| |%d| |-> ", p->data);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    DListNode* head = (DListNode*)malloc(sizeof(DListNode));
+    init(head);
+    printf("추가 단계\n");
+    for (int i = 0; i < 5; i++) {
+        insert(head, i);
+        print_list(head);
+    }
+    printf("삭제 단계\n");
+    for (int i = 0; i < 5; i++) {
+        print_list(head);
+        delete(head, head->rlink);
+    }
+    free(head);
+}
