7530b0a58e5127369ee5656ae90eaa26421a248a
JWSONG-SMU <201710866@sangmyung.kr>
2021-05-15 17:58:58 +0900

    week11

---
+++ 히프트리.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define max_element 200
+
+typedef struct
+{
+	int key;
+} element;
+
+typedef struct
+{
+	element heap[max_element];
+	int heap_size;
+} heaptype;
+
+heaptype *create()
+{
+	return (heaptype *)malloc(sizeof(heaptype));
+}
+
+void init (heaptype *h)
+{
+	h->heap_size = 0;
+}
+
+void insert_max_heap (heaptype *h, element item)
+{
+	int i;
+	i = ++(h->heap_size);
+	
+	while ((i != 1) && (item.key > h->heap[i/2].key))
+	{
+		h->heap[i] = h->heap[i/2];
+		i /= 2;
+	}
+	h->heap[i] = item;
+}
+
+element delete_max_heap (heaptype *h)
+{
+	int parent, child;
+	element item, temp;
+	
+	item = h->heap[1];
+	temp = h->heap[(h->heap_size)--];
+	parent = 1;
+	child = 2;
+	
+	while (child <= h->heap_size)
+	{
+		if ((child < h->heap_size) && 
+		(h->heap[child].key) < h->heap[child+1].key)
+			child++;
+		if (temp.key >= h->heap[child].key)
+			break;
+			
+		h->heap[parent] = h->heap[child];
+		parent = child;
+		child *= 2;
+	}
+	h->heap[parent] = temp;
+	return item;
+}
+
+int main (void)
+{
+	element e1 = {10}, e2 = {5}, e3 = {30};
+	element e4, e5, e6;
+	heaptype *heap;
+	
+	heap = create();
+	init (heap);
+	
+	insert_max_heap(heap, e1);
+	insert_max_heap(heap, e2);
+	insert_max_heap(heap, e3);
+	
+	e4 = delete_max_heap (heap);
+	printf("< %d > ", e4.key);
+	e5 = delete_max_heap (heap);
+	printf("< %d > ", e5.key);
+	e6 = delete_max_heap (heap);
+	printf("< %d > \n", e6.key);
+	
+	free (heap);
+	return 0;
+}
