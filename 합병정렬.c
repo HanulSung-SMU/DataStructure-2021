2fdec03bd68db0f14fb3136feb2be412c96f2ab5
JaeGeon-SMU <dlworjs1021@gmail.com>
2021-05-25 13:00:57 +0900

    desc

---
+++ 합병정렬.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+#define MAX_SIZE 10
+
+int list[MAX_SIZE];
+int sorted[MAX_SIZE];
+int n;
+
+void merge(int list[], int left, int mid, int right)
+{
+	int i, j, k, l;
+	i = left, j = mid + 1 , k = left;
+
+	while (i <= mid && j <= right) {
+		if (list[i] <= list[j])
+			sorted[k++] = list[i++];
+		else
+			sorted[k++] = list[j++];
+	}
+	if (i > mid)
+		for (l = j; l <= right; l++)
+			sorted[k++] = list[l];
+	else
+		for (l = i; l <= mid; l++)
+			sorted[k++] = list[l];
+	for (l = left; l <= right; l++)
+		list[l] = sorted[l];
+	}
+
+
+void merge_sort(int list[], int left, int right)
+{
+	int mid;
+	if (left < right) {
+		mid = (left + right) / 2;
+		merge_sort(list, left, mid);
+		merge_sort(list, mid + 1, right);
+		merge(list, left, mid, right);
+	}
+}
+
+int main(void) {
+	int i, j;
+	n = MAX_SIZE;
+	srand(time(NULL));
+	printf("정렬 전 : ");
+	for (i = 0; i < n; i++) {
+		list[i] = rand() % 100;
+		printf("%d ", list[i]);
+		for (j = 0; j < i; j++) {
+			while (list[i] == list[j]) {
+				list[i] = rand() % 100;
+			}
+		}
+	}
+	printf("\n정렬 후 : ");
+	merge_sort(list, 0,n-1);
+	for (i = 0; i < n; i++)
+		printf("%d ", list[i]);
+	printf("\n");
+	return 0;
+}
