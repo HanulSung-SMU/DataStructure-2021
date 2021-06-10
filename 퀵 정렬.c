34e795bc886dc5ef8e1fd74c1982f297dbc72357
202010798 <79934684+lee-jaeyu@users.noreply.github.com>
2021-06-10 22:15:13 +0900

    Add files via upload

---
+++ 퀵 정렬.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 10
+#define SWAP(x,y,t) ( (t)=(x), (x)=(y), (y)=(t) )
+
+int list[MAX_SIZE];
+int n;
+
+int partition(int list[], int left, int right)
+{
+	int pivot, temp;
+	int low, high;
+	low = left;
+	high = right + 1;
+	pivot = list[left];
+	do {
+		do
+			low++;
+		while (list[low]<pivot);
+		do
+			high--;
+		while (list[high]>pivot);
+		if (low < high) SWAP(list[low], list[high], temp);
+	} while (low < high);
+
+	SWAP(list[left], list[high], temp);
+	return high;
+}
+
+void quick_sort(int list[], int left, int right)
+{
+	if (left < right) {
+		int q = partition(list, left, right);
+		quick_sort(list, left, q - 1);
+		quick_sort(list, q + 1, right);
+	}
+}
+//
+int main(void)
+{
+	int i, n;
+	int list[MAX_SIZE];
+	n = MAX_SIZE;
+	srand(time(NULL));
+	for (i = 0; i < n; i++) 
+		list[i] = rand() % 100;
+	quick_sort(list, 0, n - 1); 
+	for (i = 0; i < n; i++)
+		printf("%d ", list[i]);
+	printf("\n");
+	return 0;
+}
