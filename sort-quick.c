c9f488634479a30ea835d31884c00f9494e2538f
gyeori0805 <gyeori0805@naver.com>
2021-06-11 14:42:06 +0900

    sort 2

---
+++ sort-quick.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 20
+#define SWAP(x, y, t) ((t) = (x), (x) = (y), (y) = (t))
+
+int list[MAX];
+int n;
+
+int partition(int list[], int left, int right)
+{
+  int pivot, temp;
+  int low, high;
+
+  low = left;
+  high = right + 1;
+  pivot = list[left];
+  do
+  {
+    do
+    {
+      low++;
+    } while(list[low] < pivot);
+    do
+    {
+      high--;
+    } while(list[high] > pivot);
+    if(low < high)
+    {
+      SWAP(list[low], list[high], temp);
+    }
+  } while(low < high);
+
+  SWAP(list[left], list[high], temp);
+  return high;
+}
+
+void quick_sort(int list[], int left, int right)
+{
+  if(left < right)
+  {
+    int q = partition(list, left, right);
+    quick_sort(list, left, q - 1);
+    quick_sort(list, q + 1, right);
+  }
+}
+
+int main(void)
+{
+  int i;
+  n = MAX;
+  srand(time(NULL));
+  for(i = 0; i < n; i++)
+  {
+    list[i] = rand() % 100;
+  }
+
+  quick_sort(list, 0, n - 1);
+  for(i = 0; i < n; i++)
+  {
+    printf("%d ", list[i]);
+  }
+  return 0;
+}
